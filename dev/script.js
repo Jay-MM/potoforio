ee43d75de33787ea3a6d6691461ad88cc30eb93f
Jesus M <marquez.jay444@gmail.com>
2024-01-19 23:01:47 -0600

    avatar image from github API

--- dev/script.js
+++ dev/script.js
@@ -1,6 +1,16 @@
 const navbar = document.getElementById("myNavbar");
+const pfp = document.getElementById("pfp");
 const navLinks = document.querySelectorAll('.navbar-nav .nav-link');
 const navbarToggler = document.querySelector('.navbar-toggler');
+
+fetch ( 'https://api.github.com/users/Jay-MM')
+.then(function (response) {
+  return response.json()
+}) 
+.then(function(data){
+  console.log(data)
+  pfp.src = data.avatar_url
+})
 
 function closeNavbar() {
   if (navbarToggler.getAttribute('aria-expanded') === 'true') {
